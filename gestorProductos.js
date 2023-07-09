16684d82b8b37ab8af406da767669898096e9d56
AnderzRomero <andersonromerorico@gmail.com>
2023-07-08 23:21:56 -0500

    se realiza ajuste en la validaciones finales, finalizando entrega

--- gestorProductos.js
+++ gestorProductos.js
@@ -7,8 +7,29 @@ class ProductManager {
 
 
     agregarProducto(titulo, descripcion, precio, imagen, codigo, existencias) {
-        ProductManager.id++
-        this.products.push({ titulo, descripcion, precio, imagen, codigo, existencias, id: ProductManager.id });
+        for (let i = 0; i < this.products.length; i++) {
+            if (this.products[i].codigo === codigo) {
+                console.log(`El codigo ${codigo} ya se encuentra agregado \n`);
+                break;
+            }
+        }
+
+
+        const nuevoProducto = {
+            titulo,
+            descripcion,
+            precio,
+            imagen,
+            codigo,
+            existencias
+        };
+
+        if (!Object.values(nuevoProducto).includes(undefined)) {
+            ProductManager.id++
+            this.products.push({ ...nuevoProducto, id: ProductManager.id });
+        } else {
+            console.log("Todos los Campos son requeridos, debe haber algun campo sin diligenciar");
+        }
     }
 
     obtenerProductos() {
@@ -21,7 +42,7 @@ class ProductManager {
 
 
     ObtenerProductosPorId(id) { !this.busquedaProducto(id) ? console.log("No Existe el producto") : console.log(this.busquedaProducto(id)) }
-    
+
 }
 
 const productos = new ProductManager
@@ -36,8 +57,10 @@ productos.agregarProducto("sirenita", "Pelicula Infantil", 2000, "imagen2", "ABC
 // Realizamos de nuevo la consulta de los productos que hay en el Array, nos debe mostrar los que agregamos
 console.log(productos.obtenerProductos(), "\n");
 
+// Agregamos un producto con un codigo repetido pero datos diferentes para validar que no se repita el Codigo
+productos.agregarProducto("Mohana", "Pelicula Infantil animada ", 5000, "imagen3", "ABC123", 7);
 
 // Buscamos el producto por id para saber si existe o no 
-productos.ObtenerProductosPorId(1);
+productos.ObtenerProductosPorId(2);
 
 
